574a62089aed71c079802dc3490b89d83d3e6211
Kierialt <holyangel.27@mail.ru>
2025-06-09 14:19:31 +0200

    changed language

--- project/src/main/java/com/example/project/Main.java
+++ project/src/main/java/com/example/project/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
         // Load login.fxml immediately for login/registration instead of main.fxml
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/project/login.fxml"));
         Scene scene = new Scene(loader.load());
-        primaryStage.setTitle("Вход в систему");
+        primaryStage.setTitle("Login to the system");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
