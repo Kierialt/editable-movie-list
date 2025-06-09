1af60a2972a4754d3af98e56bc0e9983fae83811
Kierialt <holyangel.27@mail.ru>
2025-06-09 11:51:40 +0200

    added files to folders for clear structure

--- project/src/main/java/module-info.java
+++ project/src/main/java/module-info.java
@@ -5,4 +5,10 @@ module com.example.project {
 
     opens com.example.project to javafx.fxml;
     exports com.example.project;
+    exports com.example.project.models;
+    opens com.example.project.models to javafx.fxml;
+    exports com.example.project.controllers;
+    opens com.example.project.controllers to javafx.fxml;
+    exports com.example.project.data;
+    opens com.example.project.data to javafx.fxml;
 }
